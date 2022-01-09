7b85536f9e26ac7f7826c036ef6798603b6ff648
LuanHMA <65981953+LuanHMA@users.noreply.github.com>
2022-01-09 19:15:35 -0300

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -23,3 +23,70 @@ function closeMenu(){
     menu.classList.remove("showMenu");
 }
 
+//Animações
+
+const debouce = function(func,wait,imediate){
+  let timeout;
+  return function(...args){
+    const context = this;
+    const later = function(){
+      timeout = null;
+      if(!imediate) func.apply(context, args);
+    };
+    const callNow = imediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if(callNow) func.apply(context,args)
+  };
+};
+
+const left = document.querySelectorAll("[data-left]");
+const right = document.querySelectorAll("[data-right]");
+const titles = document.querySelectorAll(".titulo");
+const scale = document.querySelectorAll("[data-scale]")
+
+function animeScroll(){
+  const windowTop = window.pageYOffset + (innerHeight*0.75);
+  //PageYoffset - Calculo em px do scroll da pagina.
+  //offsetTop - Distancia do elemento para o topo do site.
+  left.forEach(item=>{
+    if(windowTop > item.offsetTop){
+      item.classList.add("animar");
+    }else{
+      item.classList.remove("animar");
+    }
+  })
+
+  right.forEach(item=>{
+    if(windowTop > item.offsetTop){
+      item.classList.add("animar");
+    }else{
+      item.classList.remove("animar");
+    }
+  })
+
+  titles.forEach(item=>{
+    if(windowTop > item.offsetTop){
+      item.classList.add("animar");
+    }else{
+      item.classList.remove("animar");
+    }
+  })
+
+  scale.forEach(item=>{
+    if(windowTop > item.offsetTop){
+      item.classList.add("animar");
+    }else{
+      item.classList.remove("animar");
+    }
+  })
+
+}
+
+animeScroll();
+if(left.length || right.length || titles.length || scale.length){
+  window.addEventListener("scroll", debouce( function(){
+    animeScroll();
+  }, 100));
+}
+
